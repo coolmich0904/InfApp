f3e9c56c10e72902588a3b06577ebdabe887d220
Irene Lee <69160325+coolirene@users.noreply.github.com>
2021-08-27 23:45:10 -0400

    setup environment

--- InfApp/Startup.cs
+++ InfApp/Startup.cs
@@ -8,15 +8,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using InfApp.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace InfApp
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+        
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<InfAppContex>(options =>
+           {
+               options.UseSqlServer(_config.GetConnectionString("MyConnection"));
+           });
+
             services.AddMvc();
         }
 
